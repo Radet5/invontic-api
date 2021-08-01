6086febd0bcc41a34ae6a6df874c7632768c6b76
= <=>
2021-08-01 18:17:39 -0400

    users now persisted in database

--- lib/invontic_api/accounts/accounts.ex
+++ lib/invontic_api/accounts/accounts.ex
@@ -3,26 +3,22 @@ defmodule InvonticApi.Accounts do
  The Accounts Context
  """
 
+ alias InvonticApi.Repo
  alias InvonticApi.Accounts.User
 
  def list_users do
-  [
-    %User{id: "1", name: "Jose", username: "josevalim"},
-    %User{id: "2", name: "Bruce", username: "redrapids"},
-    %User{id: "3", name: "John", username: "johnnybravo"},
-    %User{id: "4", name: "Mike", username: "mikemike"},
-    %User{id: "5", name: "Sally", username: "sallysally"},
-    %User{id: "6", name: "Jane", username: "janejane"},
-  ]
+  Repo.all(User)
  end
 
  def get_user(id) do
-  Enum.find(list_users(), fn map -> map.id == id end)
+  Repo.get(User, id)
+ end
+
+ def get_user!(id) do
+  Repo.get!(User, id)
  end
 
  def get_user_by(params) do
-  Enum.find(list_users(), fn map ->
-    Enum.all?(params, fn {key, value} -> Map.get(map, key) == value end)
-  end)
+  Repo.get_by(User, params)
  end
 end
